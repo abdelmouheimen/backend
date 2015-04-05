82f55660d58744ab0fdb438ac25aa7d52b7f94c5
nairouz <nairouz.mrabah@gmail.com>
2015-04-05 02:12:17 +0200

    models "correction"

--- app/models/Vendeur.java
+++ app/models/Vendeur.java
@@ -2,15 +2,14 @@ package models;
 
 import play.db.ebean.Model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 /**
  * Created by Dell on 05/04/2015.
  */
 @Entity
+@Inheritance(strategy= InheritanceType.JOINED)
 public class Vendeur extends Model {
     @Id
     public Long id;
